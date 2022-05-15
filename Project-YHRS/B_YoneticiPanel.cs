a36b8b4ef03da1b31215dac9fa7d695f232ed0d8
Süleyman Nur Ceylan <suleymanceylan.mdbf19@iste.edu.tr>
2022-05-15 18:59:07 +0300

    Proje dosyası yeniden yüklendi.

---
+++ Project-YHRS/B_YoneticiPanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_YHRS
+{
+    public partial class B_YoneticiPanel : Form
+    {
+        public B_YoneticiPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
