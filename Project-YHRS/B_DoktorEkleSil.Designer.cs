d5c8e2bbea0acc5e06da5252d97ce3e9c9ef7502
Bilal Yücel <bilalyucel.mdbf19@iste.edu.tr>
2022-05-22 17:51:35 +0300

    Doktor Eke Sil formu tasarımı daha iyi hale getirildi.

--- Project-YHRS/B_DoktorEkleSil.Designer.cs
+++ Project-YHRS/B_DoktorEkleSil.Designer.cs
@@ -41,6 +41,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,9 +53,10 @@
             this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(1101, 12);
+            this.button6.Location = new System.Drawing.Point(657, 11);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(65, 65);
+            this.button6.Size = new System.Drawing.Size(57, 49);
             this.button6.TabIndex = 11;
             this.button6.UseVisualStyleBackColor = false;
             // 
@@ -64,9 +67,10 @@
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(1030, 12);
+            this.button5.Location = new System.Drawing.Point(595, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(65, 65);
+            this.button5.Size = new System.Drawing.Size(57, 49);
             this.button5.TabIndex = 10;
             this.button5.UseVisualStyleBackColor = false;
             // 
@@ -79,68 +83,74 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button4.Location = new System.Drawing.Point(786, 12);
+            this.button4.Location = new System.Drawing.Point(382, 11);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(238, 67);
+            this.button4.Size = new System.Drawing.Size(208, 50);
             this.button4.TabIndex = 9;
             this.button4.Text = "Profilim";
             this.button4.UseVisualStyleBackColor = false;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(190, 416);
+            this.textBox4.Location = new System.Drawing.Point(77, 273);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(315, 27);
+            this.textBox4.Size = new System.Drawing.Size(252, 23);
             this.textBox4.TabIndex = 26;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(190, 371);
+            this.textBox3.Location = new System.Drawing.Point(77, 240);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(315, 27);
+            this.textBox3.Size = new System.Drawing.Size(252, 23);
             this.textBox3.TabIndex = 25;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(190, 323);
+            this.textBox2.Location = new System.Drawing.Point(77, 203);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(315, 27);
+            this.textBox2.Size = new System.Drawing.Size(252, 23);
             this.textBox2.TabIndex = 24;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(190, 276);
+            this.textBox1.Location = new System.Drawing.Point(77, 176);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(315, 27);
+            this.textBox1.Size = new System.Drawing.Size(252, 23);
             this.textBox1.TabIndex = 23;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(110, 416);
+            this.label6.Location = new System.Drawing.Point(7, 273);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(74, 31);
+            this.label6.Size = new System.Drawing.Size(62, 25);
             this.label6.TabIndex = 22;
             this.label6.Text = "Klinik:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(25, 371);
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(25, 230);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(159, 31);
+            this.label5.Size = new System.Drawing.Size(29, 17);
             this.label5.TabIndex = 21;
-            this.label5.Text = "T.C. Kimlik No:";
+            this.label5.Text = "T.C.";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(103, 321);
+            this.label4.Location = new System.Drawing.Point(1, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 31);
+            this.label4.Size = new System.Drawing.Size(67, 25);
             this.label4.TabIndex = 20;
             this.label4.Text = "Soyad:";
             // 
@@ -148,20 +158,21 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(136, 274);
+            this.label3.Location = new System.Drawing.Point(30, 175);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 31);
+            this.label3.Size = new System.Drawing.Size(39, 25);
             this.label3.TabIndex = 19;
             this.label3.Text = "Ad:";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(552, 173);
+            this.dataGridView1.Location = new System.Drawing.Point(333, 108);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(614, 349);
+            this.dataGridView1.Size = new System.Drawing.Size(390, 284);
             this.dataGridView1.TabIndex = 27;
             // 
             // label1
@@ -169,18 +180,43 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(792, 123);
+            this.label1.Location = new System.Drawing.Point(464, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 38);
+            this.label1.Size = new System.Drawing.Size(104, 30);
             this.label1.TabIndex = 28;
             this.label1.Text = "Doktorlar";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 247);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Kimlik No";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(61, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 25);
+            this.label7.TabIndex = 30;
+            this.label7.Text = ":";
+            // 
             // B_DoktorEkleSil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
             this.BackgroundImage = global::Project_YHRS.Properties.Resources.Giriş_Yp__4_;
-            this.ClientSize = new System.Drawing.Size(1186, 543);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(726, 407);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox4);
@@ -194,7 +230,9 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "B_DoktorEkleSil";
             this.Text = "B_DoktorEkleSil";
@@ -219,5 +257,7 @@
         private Label label3;
         private DataGridView dataGridView1;
         private Label label1;
+        private Label label2;
+        private Label label7;
     }
 }
--- Project-YHRS/B_DoktorEkleSil.cs
+++ Project-YHRS/B_DoktorEkleSil.cs
@@ -16,5 +16,15 @@ namespace Project_YHRS
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
